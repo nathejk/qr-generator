5577fd5f074dd62129316ea9610c8fce0e235791
Klaus Jrgensen <klaus@blackwoodseven.com>
2016-09-08 20:59:56 +0200

    output as 8bit png

--- src/qr-generator/main.go
+++ src/qr-generator/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
-    "flag"
-    "fmt"
+	"image"
+	"image/draw"
 	"image/png"
 	"log"
 	"net/http"
@@ -19,7 +21,7 @@ func main() {
 
 	fmt.Println("starting qr-generator on port ", *port)
 	http.HandleFunc("/", QrGenerator)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil));
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
 func QrGenerator(w http.ResponseWriter, r *http.Request) {
@@ -41,24 +43,25 @@ func QrGenerator(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	size := r.URL.Query().Get("size")
-	if size == "" {
-		size = "250"
-	}
-	intsize, err := strconv.Atoi(size)
+	size, err := strconv.Atoi(r.URL.Query().Get("size"))
 	if err != nil {
-		intsize = 250
+		size = 250
 	}
 
 	// Scale the barcode to the appropriate size
-	code, err = barcode.Scale(code, intsize, intsize)
+	code, err = barcode.Scale(code, size, size)
 	if err != nil {
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
 
+	// convert to 8bit image
+	b := code.Bounds()
+	m := image.NewRGBA(b)
+	draw.Draw(m, b, code, b.Min, draw.Src)
+
 	buffer := new(bytes.Buffer)
-	if err := png.Encode(buffer, code); err != nil {
+	if err := png.Encode(buffer, m); err != nil {
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
